9bd55dadb1df59e0c069d8ecd37b9be2985429a2
Emrullah Yavuz <85703039+emrullahyavuz@users.noreply.github.com>
2025-03-04 12:41:54 +0300

    implement CORS configuration and update success message in auth controller

--- src/app.js
+++ src/app.js
@@ -1,17 +1,41 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const blogRoutes = require('./routes/blogRoutes');
 const dotenv = require('dotenv');
 
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // İzin verilen origins listesi
+    const whiteList = [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://www.google.com",
+    ];
+
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS politikası tarafından engellendiniz."));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  maxAge: 86400, // 24 saat
+};
+
+
 dotenv.config();
 
-const app = express();
-
+app.use(cors(corsOptions))
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
 app.use('/api/auth', authRoutes);
 app.use('/api/blogs', blogRoutes);
 
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,7 +7,7 @@ const register = async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     await createUser({ email, password: hashedPassword });
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: 'Kullanıcı başarıyla oluşturuldu!' });
   } catch (err) {
     res.status(500).json({ error: 'Internal server error' });
   }
